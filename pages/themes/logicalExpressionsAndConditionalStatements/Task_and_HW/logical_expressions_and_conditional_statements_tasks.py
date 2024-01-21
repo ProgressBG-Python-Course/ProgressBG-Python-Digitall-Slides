3d5af0ac7a49dcfc5d58f0703f110e87944dff5f
ProgressBG-Python-Course <ProgressBG.Python.Course@gmail.com>
2024-01-21 20:02:22 +0200

    update

--- pages/themes/logicalExpressionsAndConditionalStatements/Task_and_HW/logical_expressions_and_conditional_statements_tasks.py
+++ pages/themes/logicalExpressionsAndConditionalStatements/Task_and_HW/logical_expressions_and_conditional_statements_tasks.py
@@ -1,21 +1,11 @@
+# logical_expressions_and_conditional_statements_tasks.py
+
 # ---------------------------------- Task 1 ---------------------------------- #
 """ DESCRIPTION:
     Write a program that takes a number as input and checks if it is positive, negative, or zero.
 """
 
 ### Your code here
-def solution1():
-    number = float(input("Enter a number: "))
-
-    status = ""
-    if number == 0:
-        status = "zero"
-    elif number>0:
-        status = "positive"
-    else:
-        status = "negative"
-
-    print(f'Number {number} is {status}')
 
 ### EXPECTED OUTPUT:
 # "Enter a number: -2.45"
@@ -30,16 +20,13 @@ def solution1():
 """
 
 ### Your code here
-def solution2():
-    year = int(input("Enter a year: "))
-    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-        print("Leap year")
-    else:
-        print("Not a leap year")
 
 ### EXPECTED OUTPUT:
 # Enter a year: 2024
-# 2024 is leap year
+# 2024 is a leap year
+
+# Enter a year: 2050
+# 2050 is not leap year.
 
 
 # ---------------------------------- Task 3 ---------------------------------- #
@@ -52,22 +39,6 @@ def solution2():
 """
 
 ### Your code here
-def solution3():
-    print(f'{"Fahrenheit/Celsius Converter ":*^50s}')
-    print(f'{"# 1 => Convert to Fahrenheit":49s}#')
-    print(f'{"# 2 => Convert to Celsius":49s}#')
-    print(f'{"":*^50}')
-    choice = input('Enter your choice [1/2]: ')
-
-    input_unit = 'C' if choice=='1' else 'F'
-    output_unit = 'C' if choice=='2' else 'F'
-
-    temperature = float(input(f'Enter temperature in {input_unit}: '))
-
-    converted = (temperature*9/5) + 32 if choice=='1' else (temperature-32)*9/5
-
-    print(f'{temperature}{input_unit} = {converted}{output_unit}')
-
 
 ### EXPECTED OUTPUT:
 # **********Fahrenheit/Celsius Converter ***********
@@ -78,19 +49,8 @@ def solution3():
 # Enter temperature in C: 20
 # 20.0C = 68.0F
 
+
 # ---------------------------------- Task 4 ---------------------------------- #
-""" DESCRIPTION:
-
-"""
-
-
-### Your code here
-def solution4():
-    pass
-
-### EXPECTED OUTPUT:
-
-# ---------------------------------- Task 5 ---------------------------------- #
 """ DESCRIPTION:
     Write a Body mass index (BMI) calculator program, which asks the user for:
     weight in kilograms
@@ -118,34 +78,6 @@ def solution4():
 """
 
 ### Your code here
-def solution5():
-    w = float(input(f'Enter weight in kilograms: '))
-    h = float(input(f'Enter height in meters: '))
-
-    bmi = w/(h*h)
-
-    if bmi	< 16.0:
-        category = "Underweight (Severe thinness)"
-    elif 16.0<bmi<16.9:
-        category = "Underweight (Moderate thinness)"
-    elif 17.0<bmi<18.4:
-        category = "Underweight (Mild thinness)"
-    elif 18.5<bmi<24.9:
-        category = "Normal range"
-    elif 25.0<bmi<29.9:
-        category = "Overweight (Pre-obese)"
-    elif 30.0<bmi<34.9:
-        category = "Obese (Class I)"
-    elif 35.0<bmi<39.9:
-        category = "Obese (Class II)"
-    elif bmi>=40.0:
-        category = "Obese (Class III)"
-    else:
-        category = "None"
-
-    print(f"Your BMI = {bmi:.2f}, Category: {category}")
-
-solution5()
 
 ### EXPECTED OUTPUT:
 # Enter weight in kilograms: 92
