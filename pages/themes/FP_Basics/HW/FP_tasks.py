aba5947e0d065432d975698418179bf1b01bc21e
ProgressBG-Python-Course <ProgressBG.Python.Course@gmail.com>
2024-02-08 12:27:18 +0200

    update

---
+++ pages/themes/FP_Basics/HW/FP_tasks.py
@@ -0,0 +1,48 @@
+# ---------------------------------- Task 1 ---------------------------------- #
+""" DESCRIPTION:
+Given is a list celsius_temps = [0, 30, 100] with Celsius temperatures.
+Write a function, which will convert all temperatures in the list from Celsius to Fahrenheit.
+The formula for conversion is: Fahrenheit = (Celsius * 9/5) + 32
+"""
+### GIVEN:
+celsius_temps = [0, 30, 100]
+
+### YOUR CODE HERE:
+
+### EXPECTED OUTPUT:
+# [32.0, 86.0, 212.0]
+
+
+# ---------------------------------- Task 2 ---------------------------------- #
+""" DESCRIPTION:
+Given is a list with users data. Write a function which will return a list of all user names, for those users who have positive balance.
+"""
+
+### GIVEN:
+users = [
+    {'name':'Maria', 'balance': 2000},
+    {'name':'Petar', 'balance': -189},
+    {'name':'Ivan', 'balance': 3200},
+    {'name':'Asen', 'balance': -2890},
+]
+
+### YOUR CODE HERE
+
+### EXPECTED OUTPUT:
+# ['Maria', 'Ivan']
+
+# ---------------------------------- Task 3 ---------------------------------- #
+""" DESCRIPTION:
+Given is a list of Douglas Adams quotes. Write a function which will output in the console only the words that starts with letter 't' and are longer than 3 symbols. For simplicity, assume that words a space separated tokens.
+"""
+### GIVEN:
+quotes = [
+    'Nothing travels faster than the speed of light, with the possible exception of bad news, which obeys its own special laws',
+    'A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.'
+]
+
+### YOUR CODE HERE
+
+
+### EXPECTED OUTPUT:
+# [['travels', 'than'], ['that', 'trying']]
