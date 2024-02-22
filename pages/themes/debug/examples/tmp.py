e6bc09a04e7c3cda15eb302f150aa6bb8c9f272b
ProgressBG-Python-Course <ProgressBG.Python.Course@gmail.com>
2024-02-22 13:55:40 +0200

    update

--- pages/themes/debug/examples/tmp.py
+++ pages/themes/debug/examples/tmp.py
@@ -1,14 +1,24 @@
 import unittest
 
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
+def add(x, y):
+    return x + y
 
-class TestFactorial(unittest.TestCase):
-    def test_factorial(self):
-        self.assertEqual(factorial(5), 120)
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-2, -3), -5)
+
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(2, -3), -1)
 
 if __name__ == '__main__':
+    # If the script is run directly (not imported as a module), the unit tests are executed with:
     unittest.main()
+
+# ...
+# ----------------------------------------------------------------------
+# Ran 3 tests in 0.000s
+
+# OK
