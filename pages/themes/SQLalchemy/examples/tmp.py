5097b7b6fc2cf0967a20ec643ff10d23e1f32889
ProgressBG-Python-Course <ProgressBG.Python.Course@gmail.com>
2024-02-28 00:13:57 +0200

    done till Querying Data

--- pages/themes/SQLalchemy/examples/tmp.py
+++ pages/themes/SQLalchemy/examples/tmp.py
@@ -1,30 +1,37 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
-
 
 # Create an engine
-engine = create_engine('sqlite:///mydatabase.db', echo=True)
+engine = create_engine('sqlite:///mydatabase.db')
 
 # Declare a base using declarative_base
 Base = declarative_base()
 
 # Define the User class inheriting from Base
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String)
     age = Column(Integer)
 
-
-class Address(Base):
-    __tablename__ = 'addresses'
-    id = Column(Integer, primary_key=True)
-    email = Column(String)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship("User", back_populates="addresses")
-
 # Create the tables in the database
 Base.metadata.create_all(engine)
+
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+data = [
+    {"name": "Ivan", "age": 30},
+    {"name": "Mariya", "age": 25},
+    {"name": "Petar", "age": 35}
+]
+
+# Iterate over the list of dictionaries and add each record to the session
+for entry in data:
+    new_user = User(name=entry['name'], age=entry['age'])
+    session.add(new_user)
+
+# Commit the session to persist the changes
+session.commit()
